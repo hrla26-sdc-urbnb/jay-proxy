e267a0f8f0d89b879a1db4fe234bb940de2b8e40
JY <JY@MacBook-Pro.local>
2019-01-23 15:21:48 -0800

    staged for deployment

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const parser = require('body-parser');
+const router = require('router.js');
+
+const port = 3000;
+const app = express();
+
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true, }));
+app.use(express.static(path.join(__dirname, './public/')));
+app.use('/', router);
+
+
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log('error listening on port ', port);
+  } else {
+    console.log('successfully listening on port ', port);
+  }
+});
