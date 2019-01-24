4db1ecbe8789fc4fe6b72ce60bd6789d125931b3
JY <JY@MacBook-Pro.local>
2019-01-24 09:17:07 -0800

    added router for post

--- router.js
+++ router.js
@@ -4,12 +4,20 @@ const axios = require('axios');
 router.route('/reviews/:id')
     .get((req, res) => {
         const {id} = req.params;
-        axios.get(`http://18.188.207.136:2019/reviews/${id}`)
+        axios.get(`http://ec2-18-188-207-136.us-east-2.compute.amazonaws.com:2019/reviews/${id}`)
             .then((data) => {
                 res.send(data);
             })
             .catch(err => res.send(err));
     });
+router.route('/reviews')
+.post((req, res) => {
+    axios.post(`http://ec2-18-188-207-136.us-east-2.compute.amazonaws.com:2019/reviews`, req.body)
+        .then((data) => {
+            res.send(data);
+        })
+        .catch(err => res.send(err));
+});
 
 
 
