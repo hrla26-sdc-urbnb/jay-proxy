13df5fc5f8e776c7134f40a3937d9795f4baa0a1
JY <JY@MacBook-Pro.local>
2019-01-23 18:01:52 -0800

    refactored loaderio

--- router.js
+++ router.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 router.route('/reviews/:id')
     .get((req, res) => {
         const {id} = req.params;
-        axios.get(`18.188.207.136/reviews/${id}`)
+        axios.get(`18.188.207.136:2019/reviews/${id}`)
             .then((data) => {
                 res.send(data);
             });
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const parser = require('body-parser');
 const router = require('./router.js');
 
-const port = 3000;
+const port = 2019;
 const app = express();
 
 app.use(parser.json());
