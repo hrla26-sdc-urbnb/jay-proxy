5565a5f2a876a6229e3c6f35079226ea867fb873
JY <JY@MacBook-Pro.local>
2019-01-24 12:28:37 -0800

    removed console log

--- router.js
+++ router.js
@@ -4,7 +4,6 @@ const axios = require('axios');
 router.route('/reviews/:id')
     .get((req, res) => {
         const {id} = req.params;
-        console.log(id, 'here')
         axios.get(`http://ec2-18-188-207-136.us-east-2.compute.amazonaws.com:2019/api/reviews/${id}`)
             .then((data) => {
                 res.send(data.data);
