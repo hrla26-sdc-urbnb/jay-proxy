e15a9aae421ed2a3ed9030baef0b2ff99f2c62e9
JY <JY@MacBook-Pro.local>
2019-01-24 10:23:15 -0800

    adjustment to router

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const axios = require('axios');
 
-router.route('/reviews/:id')
+router.route('reviews/:id')
     .get((req, res) => {
         const {id} = req.params;
         axios.get(`http://ec2-18-188-207-136.us-east-2.compute.amazonaws.com:2019/reviews/${id}`)
@@ -10,7 +10,7 @@ router.route('/reviews/:id')
             })
             .catch(err => res.send(err));
     });
-router.route('/reviews')
+router.route('reviews')
 .post((req, res) => {
     axios.post(`http://ec2-18-188-207-136.us-east-2.compute.amazonaws.com:2019/reviews`, req.body)
         .then((data) => {
