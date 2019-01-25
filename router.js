00166b441cf8f81ace21460bbf5f58b8159e7961
JY <JY@MacBook-Pro.local>
2019-01-24 21:30:24 -0800

    changes to syntax for routing to balancer

--- router.js
+++ router.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 router.route('/reviews/:id')
     .get((req, res) => {
         const productid = req.params.id;
-        axios.get(`ec2-18-188-15-227.us-east-2.compute.amazonaws.com:80/api/reviews/${productid}`)
+        axios.get(`http://18.188.15.227:80/api/reviews/${productid}`)
         // axios.get(`http://localhost:2019/api/reviews/${productid}`)
             .then(({ data }) => {
                 res.send(data);
@@ -15,7 +15,7 @@ router.route('/reviews/:id')
     });
 router.route('/reviews')
 .post((req, res) => {
-    axios.post('ec2-18-188-15-227.us-east-2.compute.amazonaws.com:80/api/reviews', req.body)
+    axios.post('http://18.188.15.227:80/api/reviews', req.body)
     // axios.post('http://localhost:2019/api/reviews', req.body)
         .then(({ data }) => {
             console.log(data, 'datas')
