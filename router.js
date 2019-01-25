2046095895743ea1ea4882fb2fab528128018409
JY <JY@MacBook-Pro.local>
2019-01-24 18:46:13 -0800

    changed var name attempt to fix bug

--- router.js
+++ router.js
@@ -3,8 +3,8 @@ const axios = require('axios');
 
 router.route('/reviews/:id')
     .get((req, res) => {
-        const {id} = req.params;
-        axios.get(`http://ec2-18-188-207-136.us-east-2.compute.amazonaws.com:2019/api/reviews/${id}`)
+        const productid = req.params.id;
+        axios.get(`http://ec2-18-188-207-136.us-east-2.compute.amazonaws.com:2019/api/reviews/${productid}`)
             .then(({ data }) => {
                 res.send(data);
             })
